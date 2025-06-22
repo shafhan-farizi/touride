ea5cb6e155631eba8c82aa0e9cf97bedd3f1bd46
shafhan <shafhan.fa@gmail.com>
2025-06-22 23:20:32 +0700

    Add Favicon

--- resources/views/components/layouts/user/layouts/app.blade.php
+++ resources/views/components/layouts/user/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <title>Touride - @yield('title', 'Touride')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
 
     <!-- Bootstrap + Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
